bc8beacc1433120e1fb6a986f102588f2e21e2b5
victormmcouto <myprogrammingthings@gmail.com>
2026-01-15 14:32:33 -0300

    Adicionada classe Car

---
+++ ProblemaCalculoAluguelDeCarro/src/entities/Car.java
@@ -0,0 +1,21 @@
+package entities;
+
+public class Car {
+	
+	private String model;
+	
+	public Car() {
+	}
+	
+	public Car (String model) {
+		this.model = model;
+	}
+	
+	public String getModel() {
+		return model;
+	}
+	
+	public void setModel(String model) {
+		this.model = model;
+	}
+}
