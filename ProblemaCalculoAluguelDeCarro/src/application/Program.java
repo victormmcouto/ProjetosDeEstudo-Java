083b7c643da8e69b977935c87b0ced7734d36f0e
victormmcouto <myprogrammingthings@gmail.com>
2026-01-15 15:14:59 -0300

    Criada base da classe CarRenting e implementado método para cálculo de tempo total de aluguel

--- ProblemaCalculoAluguelDeCarro/src/application/Program.java
+++ ProblemaCalculoAluguelDeCarro/src/application/Program.java
@@ -1,10 +1,12 @@
 package application;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-
 	}
 
 }
--- ProblemaCalculoAluguelDeCarro/src/entities/CarRenting.java
+++ ProblemaCalculoAluguelDeCarro/src/entities/CarRenting.java
@@ -1,5 +1,72 @@
 package entities;
 
-public class CarRenting {
+import java.time.Duration;
+import java.time.LocalDateTime;
 
+public class CarRenting {
+	
+	public Car car;
+	public LocalDateTime pickupTime, returnTime;
+	public Double priceHour, priceDay;
+	
+	public static final Double brazilTax = 0.2;
+	
+	public CarRenting() {
+	}
+
+	public CarRenting(Car car, LocalDateTime pickupTime, LocalDateTime returnTime, Double priceHour, Double priceDay) {
+		this.car = car;
+		this.pickupTime = pickupTime;
+		this.returnTime = returnTime;
+		this.priceHour = priceHour;
+		this.priceDay = priceDay;
+	}
+
+	public Car getCar() {
+		return car;
+	}
+
+	public void setCar(Car car) {
+		this.car = car;
+	}
+
+	public LocalDateTime getPickupTime() {
+		return pickupTime;
+	}
+
+	public LocalDateTime getReturnTime() {
+		return returnTime;
+	}
+
+	public Double getPriceHour() {
+		return priceHour;
+	}
+
+	public void setPriceHour(Double priceHour) {
+		this.priceHour = priceHour;
+	}
+
+	public Double getPriceDay() {
+		return priceDay;
+	}
+
+	public void setPriceDay(Double priceDay) {
+		this.priceDay = priceDay;
+	}
+	
+	public Duration totalTime() {
+		return Duration.between(returnTime, pickupTime);
+	}
+	
+	/*public Double basicPayment() {
+		return totalTime().;
+	}
+	
+	/*public Double getTotalTax() {
+		
+	}
+	
+	public String invoice() {
+		
+	}*/
 }
