1c9d3514f73242d0db5eff50d6545b548b468ab7
victormmcouto <myprogrammingthings@gmail.com>
2026-01-20 16:07:36 -0300

    Implementada a utilização de uma interface para troca de dependencia específica de BrazilTaxService para um tipo genérico TaxService

--- ProblemaCalculoAluguelDeCarro/src/model/services/RentalService.java
+++ ProblemaCalculoAluguelDeCarro/src/model/services/RentalService.java
@@ -10,11 +10,11 @@ public class RentalService {
 	private Double pricePerHour;
 	private Double pricePerDay;
 	
-	private BrazilTaxService  taxService;
+	private TaxService  taxService;
 	
 	//Não coloca o construtor vazio para obrigar a passagem dos parÂmetros
 
-	public RentalService(Double pricePerHour, Double pricePerDay, BrazilTaxService taxService) {
+	public RentalService(Double pricePerHour, Double pricePerDay, TaxService taxService) {
 		this.pricePerHour = pricePerHour;
 		this.pricePerDay = pricePerDay;
 		this.taxService = taxService;
